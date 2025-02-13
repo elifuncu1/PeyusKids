2b390cb8af341bec47d26c9c3041090bd886d5db
elifuncu1 <99894185+elifuncu1@users.noreply.github.com>
2025-02-13 23:17:29 +0300

    <

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  devIndicators: {
+    buildActivity: false, // Yükleme göstergesini kapatır
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
